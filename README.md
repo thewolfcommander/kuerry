4860d663f3cb26509eed548e55fef38e67b05883
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-06-16 16:34:17 +0530

    Added Nuxt Project

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # kuerry
-A multipurpose design search engine
+
+> An advanced search engine for designs
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ layouts/default.vue
@@ -0,0 +1,65 @@
+<template>
+    <nav class="navbar white">
+        <v-app-bar
+            elevate-on-scroll
+            light
+            color="white"
+        >
+
+            <v-row justify="center">
+                <v-col cols="6">
+                    <v-row justify="space-between">
+                        <v-col cols="2">
+                            <v-btn text>Home</v-btn>
+                        </v-col>
+
+                        <v-col cols="2">
+                            <v-btn text>Home</v-btn>
+                        </v-col>
+
+                        <v-col cols="2">
+                            <v-btn text>Home</v-btn>
+                        </v-col>
+                    </v-row>
+                </v-col>
+            </v-row>
+
+        </v-app-bar>
+
+        <v-navigation-drawer
+            v-model="drawer"
+            absolute
+            temporary
+        >
+            <v-list
+                nav
+                dense
+            >
+                <v-list-item-group active-class="deep-purple--text text--accent-4">
+                    <v-list-item>
+                        <v-list-item-icon>
+                            <v-icon>mdi-home</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Home</v-list-item-title>
+                    </v-list-item>
+
+                    <v-list-item>
+                        <v-list-item-icon>
+                            <v-icon>mdi-account</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Account</v-list-item-title>
+                    </v-list-item>
+
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        drawer: false
+    })
+};
+</script>
